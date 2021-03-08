7269824f95d6a466d322d134868f3008f3b05c7c
eduardo <eduardormonteiro@gmail.com>
2021-03-08 16:54:15 -0300

    challenge1

---
+++ challenge1/2-odd-numbers.kt
@@ -0,0 +1,29 @@
+/* 
+
+Introdução a programação em Kotlin
+2 / 3 - Números Ímpares
+
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+8
+
+1
+3
+5
+7
+
+*/
+
+fun main(args: Array<String>) {
+    for (i in 1.rangeTo(readLine()!!.toInt()).step(2)) println(i) //complete o codigo com os valores corretos 
+ }
+
+
